374a76cb789447c179e764893b2bec045e2a032e
Victor Costa <vfcosta@gmail.com>
2015-06-12 12:08:17 -0300

    Improve dashboard layout

--- lib/gamification_plugin/dashboard_helper.rb
+++ lib/gamification_plugin/dashboard_helper.rb
@@ -3,8 +3,12 @@ module GamificationPlugin::DashboardHelper
   def level_chart(target)
 
     content_tag(:div, :class => "level pie-chart", 'data-percent' => @target.gamification_plugin_level_percent) do
-      content_tag :span, @target.level
+      content_tag :span, @target.level, :class => 'level-value'
     end
+  end
+
+  def score_point_class(point)
+    point.num_points > 0 ? 'positive' : 'negative'
   end
 
 end
