ef2edb81d2b1969e37f3b348b7a7bf79f155a2b0
Victor Costa <vfcosta@gmail.com>
2015-08-17 12:56:17 -0300

    Display positions near the target of dashboard

--- lib/gamification_plugin/dashboard_helper.rb
+++ lib/gamification_plugin/dashboard_helper.rb
@@ -20,9 +20,18 @@ module GamificationPlugin::DashboardHelper
     ranking = Profile.select('profiles.*, sum(num_points) as gamification_points, ROW_NUMBER() OVER(order by sum(num_points) DESC) as gamification_position').joins(:sash => {:scores => :score_points}).where(:type => target.class).order('sum(num_points) DESC').group('profiles.id')
     ranking = ranking.where("merit_score_points.created_at >= ?", from_date) if from_date.present?
     target_ranking = Profile.from("(#{ranking.to_sql}) profiles").where('profiles.id' => target.id).first
+
+    context_ranking = []
+    target_position = target_ranking.present? ? target_ranking.gamification_position.to_i : 0
+    if target_position > limit
+      context_limit = limit/2
+      context_ranking = ranking.offset(target_position - 1 - context_limit/2).limit(context_limit)
+      limit = limit - context_limit
+    end
     ranking = ranking.limit(limit)
 
-    render :partial => 'gamification/ranking', :locals => {:ranking => ranking, :target_ranking => target_ranking}
+    render(:partial => 'gamification/ranking', :locals => {:ranking => ranking, :target_ranking => target_ranking, :context_ranking => context_ranking}) +
+    (context_ranking.blank? ? '' : render(:partial => 'gamification/ranking', :locals => {:ranking => context_ranking, :target_ranking => target_ranking, :ranking_class => 'context'}))
   end
 
 end
