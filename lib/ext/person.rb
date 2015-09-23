09d49988711613ce616222e9651e466dd2129111
Hugo Melo <hugo@riseup.net>
2015-09-23 17:17:31 -0300

    Add methods for profile_completion rule

--- lib/ext/person.rb
+++ lib/ext/person.rb
@@ -5,4 +5,10 @@ class Person
   # TODO why this relationship doesn't exists in core?
   has_many :comments, :foreign_key => 'author_id'
 
+  def profile_completion_score_condition
+    self.points(category: 'profile_completion') == 0 and self.is_profile_complete?
+  end
+  def is_profile_complete?
+    true
+  end
 end
