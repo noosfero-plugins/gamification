004dd02a9ac3f3992cc29e9a14786a9f9bf8127a
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-09-26 20:25:14 -0300

    adapting gamification information

--- lib/gamification_plugin/api.rb
+++ lib/gamification_plugin/api.rb
@@ -9,12 +9,12 @@ class GamificationPlugin::API < Grape::API
     resource :my do 
       get 'badges' do
         authenticate!
-        present current_person.badges
+        present current_person.badges, :with => Noosfero::API::Entities::Badge
       end
 
       get 'level' do
         authenticate!
-        {:level => current_person.level, :percent => current_person.gamification_plugin_level_percent}
+        {:level => current_person.level, :percent => current_person.gamification_plugin_level_percent, :score => current_person.points}
       end
     end
 
