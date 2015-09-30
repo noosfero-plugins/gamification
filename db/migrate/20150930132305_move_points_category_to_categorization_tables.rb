aecbf9e4aa702e53f855d69b046d4b601c6ea07b
Hugo Melo <hugo@riseup.net>
2015-09-30 11:55:26 -0300

    Move seed to migration and move existing data

---
+++ db/migrate/20150930132305_move_points_category_to_categorization_tables.rb
@@ -0,0 +1,20 @@
+class MovePointsCategoryToCategorizationTables < ActiveRecord::Migration
+  def up
+  	Merit::PointRules::AVAILABLE_RULES.each do |name, setting|
+  	  type = GamificationPlugin::PointsType.create(name: name.to_s)
+  	  env = Environment.default
+  	  settings = Noosfero::Plugin::Settings.new(env, GamificationPlugin)
+      weight = settings.settings.fetch(:point_rules, {}).fetch(name.to_s, {}).fetch('weight', setting[:default_weight]).to_i
+  	  cat = GamificationPlugin::PointsCategorization.create(point_type_id: type.id, weight: weight)
+  	  Merit::Score.update_all "category = '#{cat.id}'", category: name
+  	end
+  end
+
+  def down
+  	GamificationPlugin::PointsCategorization.all.each do |categorization|
+  	  Merit::Score.update_all "category = '#{categorization.point_type.name}'", category: categorization.id.to_s
+  	  categorization.point_type.destroy
+  	  categorization.destroy
+  	end
+  end
+end
--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-unless GamificationPlugin::PointsType.count
-  Merit::PointRules::AVAILABLE_RULES.each do |name , setting|
-    GamificationPlugin::PointsType.create! name: name.to_s, description: setting[:description]
-  end
-end
