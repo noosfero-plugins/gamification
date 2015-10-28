617998d737b15d3bea32bde76d2b456be90e8f17
Victor Costa <vfcosta@gmail.com>
2015-10-28 11:17:57 -0300

    Fix identation

--- db/migrate/20150930132305_move_points_category_to_categorization_tables.rb
+++ db/migrate/20150930132305_move_points_category_to_categorization_tables.rb
@@ -1,21 +1,21 @@
 class MovePointsCategoryToCategorizationTables < ActiveRecord::Migration
   def up
-  	Merit::PointRules::AVAILABLE_RULES.each do |name, setting|
-  	  type = GamificationPlugin::PointsType.create(name: name.to_s, description: setting[:description])
-  	  env = Environment.default
+    Merit::PointRules::AVAILABLE_RULES.each do |name, setting|
+      type = GamificationPlugin::PointsType.create(name: name.to_s, description: setting[:description])
+      env = Environment.default
       next if env.blank?
-  	  settings = Noosfero::Plugin::Settings.new(env, GamificationPlugin)
+      settings = Noosfero::Plugin::Settings.new(env, GamificationPlugin)
       weight = settings.settings.fetch(:point_rules, {}).fetch(name.to_s, {}).fetch('weight', setting[:default_weight]).to_i
-  	  cat = GamificationPlugin::PointsCategorization.create(point_type_id: type.id, weight: weight)
-  	  Merit::Score.update_all "category = '#{cat.id}'", category: name
-  	end
+      cat = GamificationPlugin::PointsCategorization.create(point_type_id: type.id, weight: weight)
+      Merit::Score.update_all "category = '#{cat.id}'", category: name
+    end
   end
 
   def down
-  	GamificationPlugin::PointsCategorization.all.each do |categorization|
-  	  Merit::Score.update_all "category = '#{categorization.point_type.name}'", category: categorization.id.to_s
-  	  categorization.point_type.destroy
-  	  categorization.destroy
-  	end
+    GamificationPlugin::PointsCategorization.all.each do |categorization|
+      Merit::Score.update_all "category = '#{categorization.point_type.name}'", category: categorization.id.to_s
+      categorization.point_type.destroy
+      categorization.destroy
+    end
   end
 end
